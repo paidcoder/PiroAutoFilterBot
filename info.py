bf01263a8c3f706a373c9c32b0294c1800fddfa7
Bᴀᴅsʜᴀ Sᴛᴜᴅɪᴏs <141768309+AkdBadsha@users.noreply.github.com>
2024-01-06 08:18:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,6 +17,9 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '29909260'))
 API_HASH = environ.get('API_HASH', 'b50fc1417139325122a146ef588ec212')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
+GPLINK = environ.get('GPLINK', '')
+UPLINK = environ.get('UPLINK', '')
+
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
